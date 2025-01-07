cd1085f393f24e024e7edd2ef2d1bdd29175d4b3
Abdou Latif Ndiaye <78699994+doudou225@users.noreply.github.com>
2025-01-06 22:05:15 -0500

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -10,5 +10,5 @@ class TestApi(unittest.TestCase):
         response = self.client.get('/api/users')
         success_code = [200, 308]
         failure_message = "Failure 2.0"
-        #self.assertEqual(response.status_code, 200)
+        #self.assertEqual(response.status_code, 200) # The API first returns 308 code for redirect, followed by 200, hence line 11
         self.assertIn(response.status_code, success_code, failure_message)
